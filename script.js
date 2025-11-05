5ad935334009f6b7e44e3875e8098c2d972cf647
Baboon69 <40399361+Baboon69@users.noreply.github.com>
2025-11-06 00:23:14 +0300

    add buttons functionality

--- script.js
+++ script.js
@@ -20,21 +20,17 @@ Book.prototype.switchRead=function(){
 
 const myLibrary=[];
 
-function addBookToLibrary(title, author, pages, read){
-    const book=new Book(title, author, pages, read);
-    const id=crypto.randomUUID();
-    myLibrary.push({book, id });
-}
-
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien",295, false);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien",295, false);
-console.log(myLibrary);
-
-const myLibraryDom=document.getElementById("myLibrary");
+const myLibraryDOM=document.getElementById("myLibrary");
+const newBookButton=document.getElementById("newBookButton");
+const form=document.getElementById("bookForm");
+const dialog=document.getElementById("bookDialog");
+const closeButton=document.getElementById("closeDialog");
+const addBook=document.getElementById("addBook");
 
 function createBook(info, id){
     const book=document.createElement("div");
     book.classList.add("book");
+    book.setAttribute("data-id",id)
 
     const bookInfo=document.createElement("span");
     bookInfo.classList.add("book-info");
@@ -44,10 +40,12 @@ function createBook(info, id){
     const readButton=document.createElement("button");
     readButton.type="button";
     readButton.textContent="read switch";
+    readButton.classList.add("read-switch")
 
     const deleteButton=document.createElement("button");
     deleteButton.type="button";
     deleteButton.textContent="delete";
+    deleteButton.classList.add("delete-button");
 
     const buttonContainer=document.createElement("div");
     buttonContainer.classList.add("button-container");
@@ -60,30 +58,64 @@ function createBook(info, id){
 }
 
 function renderBooks(){
-    myLibrary.innerHtml="";
+    myLibraryDOM.innerHTML="";
     myLibrary.forEach( b =>{
-        myLibraryDom.appendChild(createBook(b.book.info(), 0));
+        myLibraryDOM.appendChild(createBook(b.book.info(), b.id));
     })
 }
-
-renderBooks();
-
-const newBookButton=document.getElementById("newBookButton");
-const form=document.getElementById("bookForm");
-const dialog=document.getElementById("bookDialog");
-const close=document.getElementById("closeDialog");
-const addBook=document.getElementById("addBook");
 
 newBookButton.addEventListener("click", ()=>{
     dialog.showModal();
 })
-addBook.addEventListener("click", (event)=>{
+/*addBook.addEventListener("click", (event)=>{
     event.preventDefault();
+    console.log(form);
+    const data=new FormData(form);
+    console.log(data);
     dialog.close();
     form.reset();
+})*/
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    console.log("form submited");
+    const data=new FormData(form);
+    const read=data.get("read")==="on";
+    addBookToLibrary(data.get("title"), data.get("author"), data.get("pages"), read);
+    dialog.close();
 })
-close.addEventListener("click", ()=>{
+closeButton.addEventListener("click", ()=>{
     dialog.close();
 })
 
-console.log(myLibraryDom)
+console.log(myLibraryDOM)
+
+function addBookToLibrary(title, author, pages, read){
+    const book=new Book(title, author, pages, read);
+    const id=crypto.randomUUID();
+    myLibrary.push({book, id });
+    renderBooks();
+}
+function removeBookFromlibrary(id){
+    myLibrary.splice(myLibrary.findIndex(book => book.id===id),1);
+    renderBooks();
+}
+function switchReadById(id){
+    const book=myLibrary.find(book => book.id===id);
+    book.book.switchRead();
+    renderBooks();
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien",295, false);
+
+myLibraryDOM.addEventListener("click", (event) => {
+    if (event.target.tagName==="BUTTON"){
+        const clickedButton=event.target;
+        const bookDOM=clickedButton.parentElement.parentElement;
+        if (clickedButton.classList.contains("delete-button")){
+            removeBookFromlibrary(bookDOM.getAttribute("data-id"));
+        }
+        if (clickedButton.classList.contains("read-switch")){
+            switchReadById(bookDOM.getAttribute("data-id"));
+        }
+    }
+})
